e90b303e8fec5f9988d9960cb92a27e4ec1ea703
Duda Carvalho <auntduda@gmail.com>
2020-04-12 17:20:10 -0300

    changing the 1045 dot cpp to one that works kinda fine, and changing the names of the files that start with the respective number

---
+++ 1045.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    float lados[3], a, b, c;
+
+    cin >> lados[0] >> lados[1] >> lados[2];
+
+    sort(lados, lados+3);
+
+    a = lados[2];
+
+    b = lados[1];
+
+    c = lados[0];
+
+    if(a >= (b+c)){
+        cout << "NAO FORMA TRIANGULO" << endl;
+
+        return 0;
+    }
+
+    if((a*a) == (b*b + c*c)){
+        cout << "TRIANGULO RETANGULO" << endl;
+    }
+
+    if((a*a) > (b*b + c*c)){
+        cout << "TRIANGULO OBTUSANGULO" << endl;
+    }
+
+    if((a*a) < (b*b + c*c)){
+        cout << "TRIANGULO ACUTANGULO" << endl;
+    }
+
+    if(a == b && b == c){
+        cout << "TRIANGULO EQUILATERO" << endl;
+    }
+
+    if(a==b || b==c || a==c){
+        cout << "TRIANGULO ISOSCELES" << endl;
+    }
+
+    return 0;
+}
